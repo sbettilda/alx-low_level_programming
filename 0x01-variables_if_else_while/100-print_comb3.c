dee90a8990b2e3dd743851ad8afe0ac8e8cf323e
Steph <stephanieamadi@gmail.com>
2023-02-18 05:47:52 +0300

    number combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,17 +8,16 @@
 
 int main(void)
 {
-	int num1;
-	int num2;
+	int digit1, digit2;
 
-	for (num1 = 0; num1 <= 10; num1++)
+	for (digit1 = 0; digit1 < 10; digit1++)
 	{
-		for (num2 = 0; num2 <= 10; num2++)
+		for (digit2 = 0; digit2 < 10; digit2++)
 		{
-			putchar((num1 % 10) + '0');
-			putchar((num2 % 10) + '0');
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
 
-			if (num1 == 9 && num2 == 9)
+			if (digit1 == 9 && digit2 == 9)
 				continue;
 
 			putchar(',');
