678f9fb58a530407e2482f142129741cb9ac6428
Steph <stephanieamadi@gmail.com>
2023-02-22 04:13:16 +0300

    Last digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,10 @@
 
 int print_last_digit(int n)
 {
-	int a;
+	int ld = n % 10;
 
-	if (n < 0)
-		n = n;
-	a = n % 10;
-	if (a < 0)
-		a = a;
-	_putchar(a + '0');
-	return (a);
+	if (ld < 0)
+		ld *= -1;
+	_putchar(ld + '0');
+	return (0);
 }
