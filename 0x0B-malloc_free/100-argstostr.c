e325aa865ab72f13da0b9b8b436cb598f42fc503
Steph <stephanieamadi@gmail.com>
2023-03-15 04:51:17 +0300

    Task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,39 +11,36 @@
 
 char *argstostr(int ac, char **av)
 {
-	int size, count, count1, count2 = 0;
+	int i, n, r = 0, l = 0;
 	char *ptr;
 
 	if (ac == 0 || av == NULL)
 	{
 		return (NULL);
 	}
-	for (count = 0; count < ac; count++)
+	for (i = 0; i < ac; i++)
 	{
-		for (count1 = 0; av[count][count1] != '\0'; count1++)
-		{
-			size += 1;
-		}
-		size += 1;
+		for (n = 0; av[i][n] != '\0'; n++)
+			l++;
 	}
-	size += 1;
 
-	ptr = malloc(sizeof(char) * size);
+	l += ac;
+
+	ptr = malloc(sizeof(char) * l + 1);
 	if (ptr == NULL)
-	{
-		free(ptr);
 		return (NULL);
-	}
-	for (count = 0; count < ac; count++)
+
+	for (i = 0; i < ac; i++)
 	{
-		for (count1 = 0; av[count][count1] != '\0'; count1++)
+		for (n = 0; av[i][n] != '\0'; n++)
 		{
-			ptr[count2] = av[count][count1];
-			count2++;
+			ptr[r] = av[i][n];
+			r++;
 		}
-		ptr[count2] = '\n';
-		count2++;
+		if (ptr[r] == '\0')
+		{
+			ptr[r++] = '\n';
+		}
 	}
-	ptr[count2] = '\0';
 	return (ptr);
 }
