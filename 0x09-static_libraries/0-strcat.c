19cbf4061602f2ad792e70aaa0f4e4352ea008a3
Steph <stephanieamadi@gmail.com>
2023-03-17 03:38:57 +0300

    My tasks

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,14 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
- /**
-  * _strcat - function that concatenates two strings
-  *
-  * @dest: input value
-  * @src: input value
-  *
-  * Return: void
-  */
+/**
+ * _strcat - function that concatenates two strings
+ *
+ * @dest: parameter defined in main
+ * @src: parameter defined in main
+ *
+ * Return: memory address of function (string)
+ */
 
 char *_strcat(char *dest, char *src)
 {
