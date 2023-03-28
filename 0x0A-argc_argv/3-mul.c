fdfe02dd1b9baa95ac82c529183d78e6e4d63cfe
Steph <stephanieamadi@gmail.com>
2023-03-29 02:41:31 +0300

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * atoi - converts a string to an integer
+ * _atoi - converts a string to an integer
  * @s: string to be converted
  *
  * Return: the int converted from the string
