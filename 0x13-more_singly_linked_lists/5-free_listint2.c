caf25e030e5e54092feae19732cf129043ea2e55
Steph <stephanieamadi@gmail.com>
2023-04-05 09:10:30 +0300

    Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,15 +8,15 @@ void free_listint2(listint_t **head)
 {
 	 listint_t *temp;
 
-	 while (*head)
-	 {
-		 temp = (*head)->next;
-		 free(*head);
-		 *head = temp;
-	 }
-	 if (head == NULL)
-	 {
-	 return;
-	 }
-	 *head = NULL;
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	if (head == NULL)
+	{
+		return;
+	}
+	*head = NULL;
 }
